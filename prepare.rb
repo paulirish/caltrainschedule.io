a491a5cce4a70a13368a0cd2fc0eb32c26088bbf
Ranmocy Sheng <ranmocy@gmail.com>
2014-09-25 11:45:58 -0700

    minify js and css

--- prepare.rb
+++ prepare.rb
@@ -17,3 +17,8 @@ times
   .map! { |s| id = s[0].split('-'); s[0] = [id[0], id[4]].join('-'); s }
 times.unshift(header)
 CSV.open("times.csv", "wb") { |c| times.each { |i| c << i } }
+
+`curl -X POST -s --data-urlencode 'input@fastclick.js' http://javascript-minifier.com/raw > fastclick.min.js`
+`curl -X POST -s --data-urlencode 'input@jquery.cookie.js' http://javascript-minifier.com/raw > jquery.cookie.min.js`
+`curl -X POST -s --data-urlencode 'input@default.js' http://javascript-minifier.com/raw > default.min.js`
+`curl -X POST -s --data-urlencode 'input@default.css' http://cssminifier.com/raw > default.min.css`
