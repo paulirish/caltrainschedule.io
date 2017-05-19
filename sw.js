dea27a80ba0cabbb4b98cdc7a426fe5aa29773a2
Paul Irish <paul.irish@gmail.com>
2017-05-18 23:53:32 -0700

    bad attempt at await

--- sw.js
+++ sw.js
@@ -1,54 +1,59 @@
 var VERSION = '19';
 
-this.addEventListener('install', function(e) {
-  e.waitUntil(caches.open(VERSION).then(cache => {
-    return cache.addAll([
+this.addEventListener('install', async function(e) {
+  const cache = await caches.open(VERSION);
+
+  await cache.addAll([
       '/',
       '/index.html',
       '/sw.js',
       '/stylesheets/default.css',
       '/javascripts/default.js'
     ]);
-  }))
 });
 
 this.addEventListener('fetch', function(e) {
-  var tryInCachesFirst = caches.open(VERSION).then(cache => {
-    return cache.match(e.request).then(response => {
-      if (!response) {
-        return handleNoCacheMatch(e);
-      }
-      // Update cache record in the background
-      fetchFromNetworkAndCache(e);
-      // Reply with stale data
-      return response
-    });
+  async function tryInCachesFirst() {
+    const cache = await caches.open(VERSION);
+    const response = await cache.match(e.request);
+
+    if (!response) {
+      return handleNoCacheMatch(e);
+    }
+    // Update cache record in the background
+    fetchFromNetworkAndCache(e);
+    // Reply with stale data
+    return response;
+  }
+  e.respondWith(tryInCachesFirst());
+});
+
+this.addEventListener('activate', async function(e) {
+  const keys = await caches.keys();
+
+  keys.forEach(async key => {
+    if (key !== VERSION)
+      await caches.delete(key);
   });
-  e.respondWith(tryInCachesFirst);
 });
 
-this.addEventListener('activate', function(e) {
-  e.waitUntil(caches.keys().then(keys => {
-    return Promise.all(keys.map(key => {
-      if (key !== VERSION)
-        return caches.delete(key);
-    }));
-  }));
-});
-
-function fetchFromNetworkAndCache(e) {
-  return fetch(e.request).then(res => {
+async function fetchFromNetworkAndCache(e) {
+  try {
+    const res = await fetch(e.request);
     // foreign requests may be res.type === 'opaque' and missing a url
     if (!res.url) return res;
     // regardless, we don't want to cache other origin's assets
     if (new URL(res.url).origin !== location.origin) return res;
 
-    return caches.open(VERSION).then(cache => {
-      // TODO: figure out if the content is new and therefore the page needs a reload.
-      cache.put(e.request, res.clone());
-      return res;
-    });
-  }).catch(err => console.error(e.request.url, err));
+    const cache = await caches.open(VERSION);
+    // TODO: figure out if the content is new and therefore the page needs a reload.
+    cache.put(e.request, res.clone());
+    return res;
+    
+  } catch (err) { 
+    console.error(e.request.url, err); 
+  }
+
 }
 
 function handleNoCacheMatch(e) {
